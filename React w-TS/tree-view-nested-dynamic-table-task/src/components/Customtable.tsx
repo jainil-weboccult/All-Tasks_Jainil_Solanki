2b00b2bd7d38729784843a0d85170eff8adabc54
jainil-weboccult <jainil.solanki@weboccult.com>
2023-04-27 17:09:47 +0530

    Cleaned Code

--- React w-TS/tree-view-nested-dynamic-table-task/src/components/Customtable.tsx
+++ React w-TS/tree-view-nested-dynamic-table-task/src/components/Customtable.tsx
@@ -15,38 +15,27 @@ import {
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import AddIcon from "@mui/icons-material/Add";
 import { v4 as uuidv4 } from "uuid";
-import { useEffect, useState } from "react";
-import Swal from "sweetalert2";
-
+import { useState } from "react";
 const nodata: string = "nodata.png";
 var uuid = uuidv4();
 interface Category {
   id: number;
   category: string;
-  values: { typeName: string; typeValue: number }[];
   subcategories?: Category[];
   parentId: number;
   currentLevel: number;
 }
-
 type Props = {
   data: Category[];
   setTableData: Function;
   item: any;
   maindata: any;
   openSubCategory: any;
-  edit: number;
   setSubOpen: Function;
-  theme: string;
-  setForceRender: Function;
-  forceRender: any;
   setData: Function;
 };
 
 export default function CustomTable(props: Props) {
-  useEffect(() => {
-    props.setForceRender(!props.forceRender);
-  }, []);
   // ------------------------------Declarations-------------------------
   const [formdata, setFormdata] = useState({
     category: "",
@@ -77,10 +66,6 @@ export default function CustomTable(props: Props) {
       const newCategory = {
         id: uuid,
         category: formdata.category,
-        // values: [],
-        values: props.item.types.map((it: { type: string }) => {
-          return { typeName: it.type, typeValue: 0 };
-        }),
         categories: [],
         subcategories: [],
         parentId: parentId,
@@ -108,194 +93,6 @@ export default function CustomTable(props: Props) {
 
     handleCloseSubCategory();
   }
-
-  // ------------------------------Function for updating values of categories -------------------------
-  function updateValue(
-    tableData: any,
-    parentId: any,
-    id: any,
-    currentLevel: any,
-    index: any,
-    value: number
-  ) {
-    const updatedCategory = [...tableData];
-    updatedCategory.forEach((category: any) => {
-      if (category.id === id) {
-        // If we found the category with the matching ID, update its values array at the specified index
-        const updatedValues = [...category.values];
-        updatedValues[index].typeValue = value;
-        category.values = updatedValues;
-        return;
-      }
-      if (category.subcategories) {
-        // If the category has subcategories, recursively call the updateValue function on each one
-
-        updateValue(
-          category.subcategories,
-          category.id,
-          id,
-          currentLevel + 1,
-          index,
-          value
-        );
-      }
-    });
-
-    const updatedItem = { ...props.item, categories: updatedCategory };
-
-    const updatedTableData = props.maindata.map((tableItem: any) =>
-      tableItem.id === props.item.id ? updatedItem : tableItem
-    );
-    props.setTableData(updatedTableData);
-  }
-
-  // ------------------------------Function for deleting categories -------------------------
-  function confirmSubCategoryDelete(tableData: any, id: any, level: number) {
-    Swal.fire({
-      title: "Are you sure?",
-      text: "You won't be able to revert this!",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
-      confirmButtonText: "Yes, delete it!",
-    }).then((result) => {
-      if (result.isConfirmed) {
-        deleteCategory(tableData, id, level);
-        Swal.fire("Deleted!", "Your file has been deleted.", "success");
-      }
-    });
-  }
-
-  function deleteCategory(tableData: any, id: any, level: number) {
-    if (level === 0) {
-      const updatedfilteredItem = props.item.categories.filter(
-        (item: any, index: number) => item.id !== id
-      );
-      const updatedItem = {
-        ...props.item,
-        categories: updatedfilteredItem,
-      };
-      const updatedTableData = props.maindata.map((tableItem: any) =>
-        tableItem.id === props.item.id ? updatedItem : tableItem
-      );
-      props.setTableData(updatedTableData);
-      props.setData(updatedItem);
-    } else {
-      const updatedCategory = [...tableData];
-
-      updatedCategory.forEach((category: any) => {
-        const finalCategory = category.subcategories.filter(
-          (item: any) => item.id !== id
-        );
-
-        if (finalCategory) {
-          category.subcategories = finalCategory;
-        }
-
-        if (category.subcategories) {
-          deleteCategory(category.subcategories, id, level + 1);
-        }
-      });
-
-      const updatedItem = { ...props.item, categories: updatedCategory };
-
-      const updatedTableData = props.maindata.map((tableItem: any) =>
-        tableItem.id === props.item.id ? updatedItem : tableItem
-      );
-      props.setTableData(updatedTableData);
-    }
-  }
-
-  // ------------------------------Function for calculating sum of values of category having subcategories -------------------------
-  function updateValuesSum(subcategory: any, index: any) {
-    if (
-      subcategory.subcategories &&
-      subcategory.values[index] &&
-      subcategory.subcategories.length > 1
-    ) {
-      let sum = 0;
-      subcategory.subcategories.forEach((item: any) => {
-        sum += item.values[index].typeValue;
-      });
-      subcategory.values[index].typeValue = sum;
-    } else if (
-      subcategory.subcategories &&
-      subcategory.subcategories.length === 1
-    ) {
-      subcategory.values[index].typeValue =
-        subcategory.subcategories[0].values[index].typeValue;
-    }
-  }
-
-  //For printing category information onlys
-  // const renderSubcategories = (subcategories: any) => {
-  //   return subcategories.map((subcategory: any, sindex: any) => {
-  //     if (subcategory.subcategories && subcategory.subcategories.length > 0) {
-  //       // Component Data to render subcategories
-  //       return (
-  //         <li key={subcategory.id}>
-  //           <span>
-  //             {subcategory.category}
-
-  //             <IconButton
-  //               aria-label="Add Another Category"
-  //               color="primary"
-  //               size="small"
-  //               onClick={(e) => {
-  //                 e.stopPropagation();
-  //                 handleOpenSubCategory(
-  //                   subcategory.id,
-  //                   subcategory.currentLevel + 1
-  //                 );
-  //               }}
-  //             >
-  //               <AddIcon fontSize="inherit" />
-  //             </IconButton>
-  //           </span>
-
-  //           <ul style={{ paddingLeft: "30px" }}>
-  //             {renderSubcategories(subcategory.subcategories)}
-  //           </ul>
-  //         </li>
-  //       );
-  //     } else {
-  //       return (
-  //         // Component data to render single input
-  //         <li key={subcategory.id}>
-  //           <span style={{ color: props.theme === "light" ? "#000" : "#fff" }}>
-  //             {subcategory.category.length > 10 ? (
-  //               <Tooltip title={subcategory.category}>
-  //                 <span>{subcategory.category.substring(0, 10)}...</span>
-  //               </Tooltip>
-  //             ) : (
-  //               subcategory.category
-  //             )}
-  //           </span>
-
-  //           {props.edit === 1 && (
-  //             <>
-  //               <IconButton
-  //                 aria-label="Add Another Category"
-  //                 color="primary"
-  //                 size="small"
-  //                 onClick={(e) => {
-  //                   e.stopPropagation();
-  //                   handleOpenSubCategory(
-  //                     subcategory.id,
-  //                     subcategory.currentLevel + 1
-  //                   );
-  //                 }}
-  //               >
-  //                 <AddIcon fontSize="inherit" />
-  //               </IconButton>
-  //             </>
-  //           )}
-  //         </li>
-  //       );
-  //     }
-  //   });
-  // };
 
   const renderSubcategories = (subcategories: any) => {
     return subcategories.map((subcategory: any, sindex: any) => {
@@ -355,7 +152,7 @@ export default function CustomTable(props: Props) {
           <li key={subcategory.id}>
             <span
               style={{
-                color: props.theme === "light" ? "#000" : "#fff",
+                color: "#000",
                 marginRight: "10px",
                 padding: "20px 30px ",
               }}
@@ -371,63 +168,46 @@ export default function CustomTable(props: Props) {
               )}
             </span>
 
-            {props.edit === 1 && (
-              <>
-                <IconButton
-                  aria-label="Add Another Category"
-                  color="primary"
-                  size="small"
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    handleOpenSubCategory(
-                      subcategory.id,
-                      subcategory.currentLevel + 1
-                    );
-                  }}
-                >
-                  <AddIcon fontSize="inherit" />
-                </IconButton>
-              </>
-            )}
+            <>
+              <IconButton
+                aria-label="Add Another Category"
+                color="primary"
+                size="small"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  handleOpenSubCategory(
+                    subcategory.id,
+                    subcategory.currentLevel + 1
+                  );
+                }}
+              >
+                <AddIcon fontSize="inherit" />
+              </IconButton>
+            </>
           </li>
         );
       }
     });
   };
 
-  // ------------------------------Functions End-------------------------
-
   // ------------------------------Component-------------------------
   return (
     <>
-      {/* <p>{props.item.categoryname}</p> */}
-
       {props.data.length > 0 ? (
         <div className="tree">{renderSubcategories(props.data)}</div>
       ) : (
+        /* No Entries Available */
         <div
           style={{
             position: "absolute",
             left: "45%",
           }}
         >
-          {/* No Entries Available */}
           <img
             src={nodata}
             alt="No Entries Available"
             style={{ height: "100px" }}
           />
-          {props.theme === "dark" && (
-            <p
-              style={{
-                color: "#fff",
-                fontSize: "10px",
-                paddingLeft: "15px",
-              }}
-            >
-              No data found
-            </p>
-          )}
         </div>
       )}
 
--- React w-TS/tree-view-nested-dynamic-table-task/src/components/Tabledata.tsx
+++ React w-TS/tree-view-nested-dynamic-table-task/src/components/Tabledata.tsx
@@ -4,40 +4,27 @@ import {
   DialogActions,
   DialogContent,
   DialogTitle,
-  IconButton,
   TextField,
 } from "@mui/material";
-import React, { useState } from "react";
-import DeleteIcon from "@mui/icons-material/Delete";
+import { useState } from "react";
 import CustomTable from "./Customtable";
 import { v4 as uuidv4 } from "uuid";
-import Swal from "sweetalert2";
 type Props = {
   key: number;
   tabledata: any;
-  theme: string;
   setTableData: Function;
   maindata: any;
-  setForceRender: Function;
-  forceRender: any;
 };
 var uuid = uuidv4();
 export default function Tabledata(props: Props) {
   // ------------------------------Declarations-------------------------
-  const [editbtn, setEditbtn] = useState(1);
-  const [formdata, setFormdata] = useState({ category: "", type: "" });
+
+  const [formdata, setFormdata] = useState({ category: "" });
   const [openCategory, setOpen] = useState(false);
   const [openSubCategory, setSubOpen] = useState(false);
-  const [openType, setOpenType] = useState(false);
+
   const handleOpenCategory = () => setOpen(true);
   const handleCloseCategory = () => setOpen(false);
-
-  const handleOpenType = () => setOpenType(true);
-  const handleCloseType = () => setOpenType(false);
-
-  let string = JSON.stringify({ ...props.tabledata });
-  let newObj = JSON.parse(string);
-  const [prevState, setPrevState] = useState(newObj);
 
   // const CustomTable states
   const [data, setData] = useState<any>({ ...props.tabledata });
@@ -56,10 +43,6 @@ export default function Tabledata(props: Props) {
         {
           id: uuid,
           category: formdata.category,
-          values: props.tabledata.types.map((it: { type: string }) => {
-            return { typeName: it.type, typeValue: 0 };
-          }),
-          //   values: [],
           categories: [],
           subcategories: [],
           parentId: props.tabledata.id,
@@ -76,168 +59,29 @@ export default function Tabledata(props: Props) {
     handleCloseCategory();
   }
 
-  // ------------------------------Function to add new type-------------------------
-  function addNewType(e: any) {
-    e.preventDefault();
-    const { type } = formdata;
-    const updatedTableData = { ...props.tabledata };
-    const { categories } = updatedTableData;
-
-    // Helper function to update values recursively
-    const updateValues = (categoryArr: any) => {
-      categoryArr.forEach((category: any) => {
-        if (category.values) {
-          category.values.push({ typeName: type, typeValue: 0 });
-        }
-        if (category.subcategories) {
-          updateValues(category.subcategories);
-        }
-      });
-    };
-
-    // Add new type to table data types array
-    updatedTableData.types.push({ type });
-
-    // Update values array for all categories at any level
-    updateValues(categories);
-
-    // Update table data in state
-    const updatedMainData = props.maindata.map((tableItem: any) =>
-      tableItem.id === updatedTableData.id ? updatedTableData : tableItem
-    );
-    props.setTableData(updatedMainData);
-    setData({ ...updatedTableData });
-    handleCloseType();
-  }
-
-  // ------------------------------Function to Delete Table from Main data-------------------------
-  function deleteTable() {
-    Swal.fire({
-      title: "Are you sure you want to delete this table?",
-      text: "You won't be able to revert this!",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
-      confirmButtonText: "Yes, delete it!",
-    }).then((result: any) => {
-      if (result.isConfirmed) {
-        const updatedTableData = props.maindata.filter(
-          (tableItem: any) => tableItem.id !== props.tabledata.id
-        );
-        props.setTableData(updatedTableData);
-        Swal.fire("Deleted!", "Table has been deleted.", "success");
-      }
-    });
-  }
-
-  // ------------------------------Function for edit button-------------------------
-  const setEditList = () => {
-    setEditbtn(1);
-    let string = JSON.stringify({ ...props.tabledata });
-    let newObj = JSON.parse(string);
-    setPrevState(newObj);
-  };
-
-  // ------------------------------Function to save edits-------------------------
-  function saveEdit() {
-    setEditbtn(0);
-    setPrevState({ ...props.tabledata });
-  }
-
-  // ------------------------------Function to cancel edits-------------------------
-  function cancelEdit() {
-    setEditbtn(0);
-
-    const updatedTableData = props.maindata.map((tableItem: any) =>
-      tableItem.id === props.tabledata.id ? prevState : tableItem
-    );
-    props.setTableData([...updatedTableData]);
-    setData({ ...prevState });
-  }
-
-  // ------------------------------Functions End-------------------------
-
-  // ------------------------------Component-------------------------
   return (
     <div className="container">
       <div className="table-header">
-        {/* <div className="table-title">
-          <h1
-            style={{
-              color: props.theme === "light" ? "rgba(0, 0, 0, 0.87) " : "#fff",
-            }}
-            className="table-heading"
-          >
-            {props.tabledata.tablename}
-          </h1>
-          {editbtn === 1 && (
-            <IconButton
-              aria-label="delete"
-              color="error"
-              size="small"
-              onClick={() => deleteTable()}
-            >
-              <DeleteIcon fontSize="inherit" />
-            </IconButton>
-          )}
-        </div> */}
-
-        {editbtn === 1 && (
-          <div className="btn-list">
-            <Button
-              variant="contained"
-              className="edit-btn"
-              onClick={handleOpenCategory}
-              color="secondary"
-            >
-              Add Category Row
-            </Button>
-
-            {/* <Button
-              variant="contained"
-              className="edit-btn"
-              color="success"
-              onClick={() => saveEdit()}
-            >
-              Save
-            </Button>
-            <Button
-              variant="contained"
-              className="edit-btn"
-              color="error"
-              onClick={() => cancelEdit()}
-            >
-              Cancel
-            </Button> */}
-          </div>
-        )}
-
-        {editbtn === 0 && (
+        <div className="btn-list">
           <Button
             variant="contained"
             className="edit-btn"
-            onClick={setEditList}
+            onClick={handleOpenCategory}
+            color="secondary"
           >
-            Edit
+            Add Category Row
           </Button>
-        )}
+        </div>
       </div>
       <br />
 
       <CustomTable
-        // data={props.tabledata.categories}
         data={data.categories}
         setTableData={props.setTableData}
-        // item={props.tabledata}
         item={data}
         maindata={props.maindata}
         openSubCategory={openSubCategory}
         setSubOpen={setSubOpen}
-        edit={editbtn}
-        theme={props.theme}
-        setForceRender={props.setForceRender}
-        forceRender={props.forceRender}
         setData={setData}
       />
       {/* //Category Dialog */}
